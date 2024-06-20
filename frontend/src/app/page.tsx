4bae88b53689d8bbc62278afadd658471c60e962
Jaewon Jeon <wksk04515@naver.com>
2024-06-21 03:02:46 +0900

    chore: Add Tailwind CSS configuration files and update npm dependencies

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css'
+
+const coffeeItems = [
+  {
+    name: 'Espresso',
+    price: '3,000원',
+    vendor: 'Starbucks',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+  {
+    name: 'Cappuccino',
+    price: '3,000원',
+    vendor: 'Costa',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+  {
+    name: 'Latte',
+    price: '3,000원',
+    vendor: 'Dunkin',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+  {
+    name: 'Espresso',
+    price: '3,000원',
+    vendor: 'Starbucks',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+  {
+    name: 'Cappuccino',
+    price: '3,000원',
+    vendor: 'Costa',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+  {
+    name: 'Latte',
+    price: '3,000원',
+    vendor: 'Dunkin',
+    href: 'https://www.starbucks.co.kr/menu/drink_list.do',
+  },
+]
+
+const CoffeeItem = ({ item }: any) => (
+  <a href={item.href} target="_blank" className="border border-black rounded shadow-md flex flex-col cursor-pointer">
+    <img src="https://picsum.photos/200/200" className="w-full" />
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+      <h2 className="text-xl font-bold text-gray-800 mb-2">{item.name}</h2>
+      <p className="text-lg text-gray-600 mb-1 font-semibold">{item.price}</p>
+      <p className="text-sm text-gray-600 font-semibold text-end">{item.vendor}</p>
+    </div>
+  </a>
+)
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="grid grid-cols-5 gap-4">
+        {coffeeItems.map((item, index) => (
+          <CoffeeItem key={index} item={item} />
+        ))}
       </div>
     </main>
-  );
+  )
 }
