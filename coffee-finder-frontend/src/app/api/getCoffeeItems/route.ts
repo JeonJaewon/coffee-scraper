5786f16a7f03777e3679dd68368b19340dfca3ec
Jaewon Jeon <wksk04515@naver.com>
2024-07-17 02:43:39 +0900

    Add try-catch statement to api/getCoffeeItems

--- coffee-finder-frontend/src/app/api/getCoffeeItems/route.ts
+++ coffee-finder-frontend/src/app/api/getCoffeeItems/route.ts
@@ -3,19 +3,23 @@ import { VendorSnapshots } from 'coffee-scraper/src/types'
 import { collection, getDocs, limit, orderBy, query, where } from 'firebase/firestore'
 
 export async function GET() {
-  const queryLatestTimestamp = query(collection(db, 'createdAtTimestamps'), orderBy('createdAt', 'desc'), limit(1))
-  const latestTimestampDocs = await getDocs(queryLatestTimestamp)
-  const latestCreatedAt = latestTimestampDocs.docs[0].data().createdAt
+  try {
+    const queryLatestTimestamp = query(collection(db, 'createdAtTimestamps'), orderBy('createdAt', 'desc'), limit(1))
+    const latestTimestampDocs = await getDocs(queryLatestTimestamp)
+    const latestCreatedAt = latestTimestampDocs.docs[0].data().createdAt
 
-  const queryLatestSnapshot = query(collection(db, 'vendorSnapshots'), where('createdAt', '==', latestCreatedAt))
-  const snapShot = await getDocs(queryLatestSnapshot)
+    const queryLatestSnapshot = query(collection(db, 'vendorSnapshots'), where('createdAt', '==', latestCreatedAt))
+    const snapShot = await getDocs(queryLatestSnapshot)
 
-  // TODO replace 'as' with type converter
-  const coffeeItems = snapShot.docs
-    .map((doc) => doc.data() as VendorSnapshots)
-    .map((vendorSnapshot) => Object.values(vendorSnapshot.items))
-    .flat()
-    .flat()
+    // TODO replace 'as' with type converter
+    const coffeeItems = snapShot.docs
+      .map((doc) => doc.data() as VendorSnapshots)
+      .map((vendorSnapshot) => Object.values(vendorSnapshot.items))
+      .flat()
+      .flat()
 
-  return Response.json(coffeeItems)
+    return Response.json(coffeeItems)
+  } catch (error) {
+    return Response.error()
+  }
 }
