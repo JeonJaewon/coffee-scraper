041831b212dc7ac7a828520654610f490a3da19e
Jaewon Jeon <wksk04515@naver.com>
2024-07-26 03:14:56 +0900

    Add empty list

--- coffee-finder-frontend/src/app/components/CoffeeGrid.tsx
+++ coffee-finder-frontend/src/app/components/CoffeeGrid.tsx
@@ -9,8 +9,11 @@ export const CoffeeGrid = () => {
   const { data: coffeeItems } = useCoffeeItemsQuery()
   const filteredCoffeeItems = useFilterCoffeeItems(coffeeItems)
 
+  const isEmpty = filteredCoffeeItems.length === 0
+
   return (
-    <div className="w-full max-w-7xl p-2 grid gap-8 grid-cols-[repeat(auto-fill,minmax(18rem,1fr))]">
+    <div className="w-full max-w-7xl py-4 px-12 grid gap-8 grid-cols-[repeat(auto-fill,minmax(12rem,1fr))]">
+      {isEmpty && <p className="text-center text-lg text-gray-800">검색 결과가 없습니다.</p>}
       {filteredCoffeeItems.map((item) => (
         <CoffeeGridItem key={item.url} {...item} />
       ))}
@@ -20,8 +23,8 @@ export const CoffeeGrid = () => {
 
 const CoffeeGridItem = ({ thumbnailSrc, title, price, url, vendorName }: CoffeeItem) => (
   <a href={url} target="_blank" className="rounded shadow-lg flex flex-col cursor-pointer overflow-hidden">
-    <img src={thumbnailSrc} className="w-full h-full object-cover bg-white" loading="lazy" />
-    <div className="flex flex-col p-4 bg-gray-100 rounded-b-lg h-2/5">
+    <img src={thumbnailSrc} className="flex-1 object-cover aspect-square p-1 bg-white" loading="lazy" />
+    <div className="flex flex-col flex-1 p-4 bg-gray-100 rounded-b-lg h-2/5">
       <h2 className="text-base font-bold text-gray-800 mb-2 overflow-hidden text-ellipsis line-clamp-2">{title}</h2>
       <div className="mt-auto">
         <p className="text-md text-gray-800 mb-1 font-semibold">{formatToKRW(price)}</p>
